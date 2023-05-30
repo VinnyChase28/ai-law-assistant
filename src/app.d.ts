e1c1120808277f500a8222d00ebed23fe5875a74
VinnyChase28 <vincentgauthier068@gmail.com>
2023-05-29 22:19:30 -0700

    add types for keycloak

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,19 +1,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		interface Supabase {
-			Database: import('./DatabaseDefinitions').Database;
-			SchemaName: 'public';
-		}
-		interface PageData {
-			session: import('@supabase/supabase-js').Session | null;
-		}
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface ImportMetaEnv {
+      VITE_OIDC_ISSUER: string;
+      VITE_OIDC_CLIENT_ID: string;
+      VITE_OIDC_CLIENT_SECRET: string;
+      VITE_OIDC_REDIRECT_URI: string;
+      VITE_OIDC_POST_LOGOUT_REDIRECT_URI: string;
+      VITE_OIDC_CLIENT_SCOPE: string;
+      VITE_OIDC_TOKEN_REFRESH_MAX_RETRIES: number;
+      VITE_REFRESH_TOKEN_ENDPOINT: string;
+      VITE_REFRESH_PAGE_ON_SESSION_TIMEOUT: boolean;
+    }
+
+    interface PageData {
+      session: import("sveltekit-oidc").Session | null;
+    }
+  }
 }
 
 export {};
